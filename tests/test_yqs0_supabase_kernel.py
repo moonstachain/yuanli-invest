4a11166b261b4a2a5457ae5934a65ff4a5a2088c
HAY2045 <48938217@qq.com>
2026-08-26 12:19:34 +0800

    test(yqs0): make architecture tests unittest-discoverable

--- tests/test_yqs0_supabase_kernel.py
+++ tests/test_yqs0_supabase_kernel.py
@@ -1,3 +1,5 @@
+import unittest
+
 from scripts.validate_yqs0_supabase_kernel import (
     validate_architecture_text,
     validate_inventory,
@@ -8,25 +10,25 @@ from scripts.validate_yqs0_supabase_kernel import (
 )
 
 
-def test_yqs0_inventory_contract():
-    validate_inventory()
+class YQS0SupabaseKernelTests(unittest.TestCase):
+    def test_inventory_contract(self):
+        validate_inventory()
+
+    def test_pit_law(self):
+        validate_pit_law()
+
+    def test_rls_authority_matrix(self):
+        validate_rls()
+
+    def test_quant_worker_contract(self):
+        validate_worker_contract()
+
+    def test_stage_authority_boundaries(self):
+        validate_stage_state()
+
+    def test_architecture_text_invariants(self):
+        validate_architecture_text()
 
 
-def test_yqs0_pit_law():
-    validate_pit_law()
-
-
-def test_yqs0_rls_authority_matrix():
-    validate_rls()
-
-
-def test_yqs0_quant_worker_contract():
-    validate_worker_contract()
-
-
-def test_yqs0_stage_authority_boundaries():
-    validate_stage_state()
-
-
-def test_yqs0_architecture_text_invariants():
-    validate_architecture_text()
+if __name__ == "__main__":
+    unittest.main()
